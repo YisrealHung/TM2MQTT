f42632e0b1defe8420e1677ffafafa6001316ae7
YisrealHung <43162482+YisrealHung@users.noreply.github.com>
2021-05-13 09:01:51 +0800

    Add files via upload

--- vision/vision.js
+++ vision/vision.js
@@ -1,33 +1,30 @@
-let model, webcam, labelContainer, maxPredictions, position = -1, range, open = 0;
+let model, webcam, labelContainer, maxPredictions, position = -1, range, check = -1, lastrange = -1;
 var target_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 async function init() {
-    if (open == 0) {
-      open = 1;
-      const URL = document.getElementById("modelin").value;
-      const modelURL = URL + "model.json";
-      const metadataURL = URL + "metadata.json";
+    const URL = document.getElementById("modelin").value;
+    const modelURL = URL + "model.json";
+    const metadataURL = URL + "metadata.json";
 
-      model = await tmImage.load(modelURL, metadataURL);
-      maxPredictions = model.getTotalClasses();
+    model = await tmImage.load(modelURL, metadataURL);
+    maxPredictions = model.getTotalClasses();
 
-      const flip = true;
+    const flip = true;
 
-      webcam = new tmImage.Webcam(640, 640, flip);
+    webcam = new tmImage.Webcam(640, 640, flip);
 
 
-      await webcam.setup();
-      await webcam.play();
+    await webcam.setup();
+    await webcam.play();
 
-      window.requestAnimationFrame(loop);
+    window.requestAnimationFrame(loop);
 
-      document.getElementById("webcam-container").appendChild(webcam.canvas);
+    document.getElementById("webcam-container").appendChild(webcam.canvas);
 
-      for (let i = 0; i < maxPredictions; i++) {
-          var tt = "myProgress" + (i+1);
-          var elem = document.getElementById(tt);
-          elem.style.width = 200 + 'pt';
-      }
+    for (let i = 0; i < maxPredictions; i++) {
+        var tt = "myProgress" + (i+1);
+        var elem = document.getElementById(tt);
+        elem.style.width = 200 + 'pt';
     }
 }
 
@@ -46,16 +43,22 @@ async function predict() {
         if (range < prediction[i].probability.toFixed(2)) {
             range = prediction[i].probability.toFixed(2);
             position = i;
+            lastrange = range;
         }
         var elem = document.getElementById("myBar"+target_list[i]);
         elem.style.width = prediction[i].probability.toFixed(2)*100 + '%';
         document.getElementById("myBar"+target_list[i]).innerHTML= (prediction[i].probability.toFixed(2)*100).toFixed(0) + '%';
         document.getElementById("label"+target_list[i]).innerHTML= prediction[i].className;
     }
-    try {
-        publish(String(position + 1));
-    } catch (error) {
-        console.error(error);
+    if (lastrange > 0.7) {
+      if (position != check) {
+          try {
+              publish(String(position + 1));
+              check = position;
+          } catch (error) {
+              // pass
+          }
+      }
     }
     range = -1;
 }
@@ -108,7 +111,7 @@ function onConnectionLost(responseObject) {
 
 // Called when a message arrives
 function onMessageArrived(message) {
-    console.log("onMessageArrived: " + message.payloadString);
+    // console.log("onMessageArrived: " + message.payloadString);
     document.getElementById("messages").innerHTML += '<span>' + message.destinationName + ' : ' + message.payloadString + '</span><br/>';
     updateScroll(); // Scroll to bottom of window
 }
